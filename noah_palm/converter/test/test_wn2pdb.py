4b82e7283e67c1a1c517d8071790667dfff40622
cvs <cvs@e2220a44-86da-0310-87c7-a54c3a5c9fad>
2003-09-23 01:32:03 +0000

    updated unit tests
    git-svn-id: https://repo.kowalczyk.info/svn/noah_palm/trunk@97 e2220a44-86da-0310-87c7-a54c3a5c9fad

--- noah_palm/converter/test/test_wn2pdb.py
+++ noah_palm/converter/test/test_wn2pdb.py
@@ -1,9 +1,10 @@
-import sys
+import sys,os
 sys.path.append("..")
 sys.path.append(".")
 
 import unittest
 import wn2pdb
+
 
 class Wn2PdbTest(unittest.TestCase):
 
@@ -34,20 +35,56 @@ class Wn2PdbTest(unittest.TestCase):
                  ['google','googleme',6] ]
         for t in data:
             self.assertEqual(wn2pdb._commonPrefixLen(t[0],t[1]),t[2])
-            self.assertEqual(wn2pdb._commonPrefixLen2(t[0],t[1]),t[2])
 
     def test_compressedWordsGen(self):
         # Undone: test _PREFIX_LEN_LIMIT
         words =           [ "a", "abcd",    "abru",  "abru", "beh", "", "beh", "bral"]
         compressedWords = [ "a", "\x01bcd", "\x02ru","\x04", "beh", "", "beh", "\x01ral" ]
-        myCompressedWords = wn2pdb.CompressedWordsGen(words)
+        myCompressedWords = wn2pdb.compressedWordsGen(words)
         self.assertEqual( words, words )
-        for x,y in zip(compressedWords, myCompressedWords):
-            self.assertEqual(x,y)
+        for word,wordCompressed in zip(compressedWords, myCompressedWords):
+            self.assertEqual(word,wordCompressed)
         # now test WordCompressor class
         wc = wn2pdb.WordCompressor()
-        for w,wCompressed in zip(words,compressedWords):
-            self.assertEqual(wc.compressWord(w),wCompressed)
+        for word,wordCompressed in zip(words,compressedWords):
+            self.assertEqual(wc.compress(word),wordCompressed)
+
+    def _testCompressionWithCompressor(self,compressor,lines):
+        packStrings = compressor.buildPackStrings()
+        self.assertEqual(256,len(packStrings))
+        comp = wn2pdb.StringCompressor(packStrings)
+        uncompressedSize = 0
+        compressedSize = 0
+        for line in lines:
+            uncompressedSize += len(line)
+            compressedLine = comp.compress(line)
+            compressedSize += len(compressedLine)
+            uncompressedLine = comp.uncompress(compressedLine)
+            self.assertEqual(line,uncompressedLine)
+        return (uncompressedSize, compressedSize)
+        
+    def test_compression(self):
+        # to test compression we'll compress/decompress a large text file
+        # i.e. wn2pdb.py script itself
+        lines = getLinesFromFile()
+        comp = wn2pdb.CompInfoGen(lines)
+        compressionOne = self._testCompressionWithCompressor(comp,lines)
+        print "\ncompressed size for CompInfoGen     : %d" % compressionOne[1]
+        comp = wn2pdb.CompInfoGenWeak(lines)
+        compressionTwo = self._testCompressionWithCompressor(comp,lines)
+        print "compressed size for CompInfoGenWeak : %d" % compressionTwo[1]
+        #comp = wn2pdb.CompInfoGenOrig(lines)
+        #compressionThree = self._testCompressionWithCompressor(comp, lines)
+        #print "compressed size for CompInfoGenOrig: %d" % compressionThree[1]
+        assert compressionOne[0] == compressionTwo[0]
+        #assert comprssionTwo[0] == compressionThree[0]
+
+def getLinesFromFile():
+    fileName = os.path.join("..", "wn2pdb.py")
+    fo = open(fileName, "rb")
+    lines = fo.readlines()
+    fo.close()
+    return lines
 
 if __name__ == '__main__':
     unittest.main()
